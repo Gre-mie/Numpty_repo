960166ade3324463660a9e577338b44f3d30c534
Gre-mie <meow31-cat@outlook.com>
2024-10-22 21:45:46 +0100

    Add: fishcake class, prints a fishcake to the terminal

--- weekly_projects/python/2024_10_20_Cake.py
+++ weekly_projects/python/2024_10_20_Cake.py
@@ -1,20 +1,27 @@
 # Draw cakes to the terminal, simple programe without inputs
 
-# prototypes                                                description
+# prototypes
+# Fishcake(self, width) 
+
+# -- prototypes incomplete --                               -- description --
 # Victoria_Sponge(self, width)                              sponge, jam, cream, sponge
 # Chocolate_Cake(self, width, int_layers, bool_icing)       icing (if true), sponge, (filling, sponge, ... n layers -loop)
 # Vanilla_Cake(self, width, int_layers, string_topping)     topping ("cherries", "candals", "None" -default) evenly distributed across width, icing (with a drip down onto next layer, half unit), sponge, (jam, cream, sponge, ... n layers -loop)
+
+# boot.dev joke cakes
+# Fish_barrel_cake(self, width, int_layer, fish_colour)     fish tail (in center, may need special chars), sponge, (filling (same as Fishcake), sponge, ... n layers -loop) 
 
 # Note:
 #   candal wick can be '|'
 #   candal wax can be half_unit
 
-# constants
+# ----- constants -----
 # f"\033[38;2;{};{};{}m"
 # f"\033[48;2;{};{};{}m"
 CHAR_COLOURS = {
     "default": f"\033[39m", 
 #    "white": f"\033[38;2;{255};{255};{255}m",
+    "orange": f"\033[38;2;{220};{129};{39}m"
 #    "dark_red": f"\033[38;2;{172};{50};{50}m", 
 #    "light_red": f"\033[38;2;{217};{87};{99}m"
 }
@@ -22,8 +29,34 @@ BG_COLOURS = {
     "default": f"\033[49m", 
 #    "cream_brown": f"\033[48;2;{238};{195};{154}m",
 #    "cream": f"\033[48;2;{251};{245};{225}m",
+    "orange": f"\033[48;2;{220};{129};{39}m"
 #    "dark_red": f"\033[48;2;{172};{50};{50}m"
 }
+
+# ----- class definitions -----
+
+# One unit    = 2 chars
+# Half a unit = 1 char
+
+class Fishcake:
+    def __init__(self, unit_width):
+        self.width = unit_width
+    
+    def print_cake(self):
+        start_cake = [CHAR_COLOURS["orange"], '(']
+        end_cake = [')', CHAR_COLOURS["default"], '\n']
+        cake = start_cake
+
+        for _ in range(self.width):
+            cake.append(BG_COLOURS["orange"])
+            cake.append("  ")
+            cake.append(BG_COLOURS["default"])
+        cake.append("".join(end_cake))
+
+        print("".join(cake))
+
+
+# ----- prototype -----
 
 # layer function
 #def sponge_layer(width, height, col, char):
@@ -45,9 +78,14 @@ BG_COLOURS = {
     
 #    print(''.join(sponge))
 
+# ----- global functions -----
 
 def main():
-    print() # test
+    print()
+
+    fishcake = Fishcake(5)
+    fishcake.print_cake()
+
     # sponge_layer(unit_width, unit_height, colour, )
     # 1 unit = 2 spaces
     # half a unit = 1 space
